2b250afc192dde92ef8d93e29881da552cfbb6e0
Elaidzha1940 <eli080293@gmail.com>
2023-09-25 20:40:36 +0300

    MapKit

---
+++ MapKit/ContentView.swift
@@ -0,0 +1,24 @@
+//  /*
+//
+//  Project: MapKit
+//  File: ContentView.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 25.09.2023
+//
+//  */
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        VStack {
+           
+            
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MapKit/MapKitApp.swift
@@ -0,0 +1,19 @@
+//  /*
+//
+//  Project: MapKit
+//  File: ContentView.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 25.09.2023
+//
+//  */
+
+import SwiftUI
+
+@main
+struct MapKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
